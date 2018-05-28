e242060e58e75b06c0a637e045d7386330ed7ff6
Ramen guchhait <ramen2k10@gmail.com>
2018-05-28 23:39:52 +0530

    Hello.java modified

--- Hello.java
+++ Hello.java
@@ -1,5 +1,8 @@
 class Hello{
    public static void main(String args[]){
-       System.out.println("Hello Ramen");
+     for(int i=0;i<=5;i++){
+           System.out.println("Hello Ramen");
+       }
+       
     }
 }
